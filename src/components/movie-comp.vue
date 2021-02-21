453f388d878026c6b84469909cfb2cf15fa6462d
Marlon Lavanda <marlonlavanda@hotmail.com>
2021-02-21 23:04:57 +0000

    Creación del componente movies y uso de props

---
+++ src/components/movie-comp.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <img :src="cover" />
+    <h2 v-text="title"></h2>
+    <p v-text="synopsis"></p>
+    <hr />
+  </div>
+</template>
+<script>
+export default {
+  name: "MovieComp",
+  props: {
+    id: {
+      type: Number,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    synopsis: {
+      type: String,
+      required: true
+    },
+    cover: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
---
+++ src/views/movies.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>Películas</h1>
+    <MovieComp
+      v-for="movie in movies"
+      :key="movie.id"
+      :id="movie.id"
+      :title="movie.title"
+      :synopsis="movie.synopsis"
+      :cover="movie.cover"
+    ></MovieComp>
+  </div>
+</template>
+
+<script>
+import MovieComp from "@/components/movie-comp.vue";
+export default {
+  components: { MovieComp },
+  data() {
+    return {
+      movies: [
+        {
+          id: 1,
+          title: "Titanic",
+          synopsis:
+            "Seventeen-year-old Rose hails from an aristocratic family and is set to be married. When she boards the Titanic, she meets Jack Dawson, an artist, and falls in love with him.",
+          cover:
+            "https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_UY1200_CR88,0,630,1200_AL_.jpg"
+        },
+        {
+          id: 2,
+          title: "El rey León",
+          synopsis:
+            "Seventeen-year-old Rose hails from an aristocratic family and is set to be married. When she boards the Titanic, she meets Jack Dawson, an artist, and falls in love with him.",
+          cover:
+            "https://i.pinimg.com/originals/b7/82/fc/b782fc749af22348e676ffe771e2f18e.jpg"
+        },
+        {
+          id: 3,
+          title: "Avengers: Endgame",
+          synopsis:
+            "Seventeen-year-old Rose hails from an aristocratic family and is set to be married. When she boards the Titanic, she meets Jack Dawson, an artist, and falls in love with him.",
+          cover:
+            "https://images-na.ssl-images-amazon.com/images/I/81%2BNup8-8NL._AC_SL1500_.jpg"
+        }
+      ]
+    };
+  }
+};
+</script>
